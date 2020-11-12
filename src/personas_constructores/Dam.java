988563e1f9bdbd6cd603ad72a731bd15abc2a5ce
NitroPC <NitroPC@David.Home>
2020-11-12 11:43:14 +0100

    el final

--- src/personas_constructores/Dam.java
+++ src/personas_constructores/Dam.java
@@ -4,11 +4,11 @@ public class Dam {
 
 	public static void main(String[] args) {
 		
-		Personas miCarlota= new Personas(171, 53, null);// no se por que se pone null
-		Personas miSara= new Personas(165, 50, null);
-		Personas miDiego= new Personas(175, 59, null);
+		Personas Carlota= new Personas(171, 53,"marron");// no se por que se pone null
+		Personas Sara= new Personas(165, 50, "marron");
+		Personas Diego= new Personas(175, 59,"azul");
 		
-		System.out.println(miCarlota);// mo se imprime
+		System.out.println(Carlota);// mo se imprime
 	}
 
 }
