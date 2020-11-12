007b4f6f5f6b28e78c5496adf893c59f8c83f313
NitroPC <NitroPC@David.Home>
2020-11-12 11:38:33 +0100

    prueba de constructor

---
+++ src/personas_constructores/Dam.java
@@ -0,0 +1,14 @@
+package personas_constructores;
+
+public class Dam {
+
+	public static void main(String[] args) {
+		
+		Personas miCarlota= new Personas(171, 53, null);// no se por que se pone null
+		Personas miSara= new Personas(165, 50, null);
+		Personas miDiego= new Personas(175, 59, null);
+		
+		System.out.println(miCarlota);// mo se imprime
+	}
+
+}
---
+++ src/personas_constructores/Personas.java
@@ -0,0 +1,17 @@
+package personas_constructores;
+
+public class Personas {
+	private String colorojos; // atributos private y no poner valor si no todas las personas igual.
+	private double estatura;
+	private double peso;
+
+	public Personas(double estatura , double peso,String colorojos){
+	
+           colorojos= colorojos; 
+           estatura=0; // esto es 0 por que luego toma ek valor que tu le pongas
+	       peso=0;
+	
+	
+	}
+
+}
